0b34fba813589a2a159fcf984321ee3f4cd10ec0
Josh Purcell <hello@vuld.in>
2022-10-24 19:07:54 -0400

    cloud_storage_credentials_source to aws_instance_metadata

--- redpanda-node.sh
+++ redpanda-node.sh
@@ -11,15 +11,12 @@ apt install -y redpanda
 INTERNAL_IP=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
 
 sudo -u redpanda rpk redpanda config bootstrap --id 0 --self $INTERNAL_IP
-
 sudo -u redpanda rpk redpanda config set cluster_id 'test-cluster'
 sudo -u redpanda rpk redpanda config set organization 'test-org'
 sudo -u redpanda rpk redpanda config set redpanda.advertised_kafka_api "[{address: $INTERNAL_IP, port: 9092}]"
 sudo -u redpanda rpk redpanda config set redpanda.advertised_rpc_api "{address: $INTERNAL_IP, port: 33145}"
 
 systemctl start redpanda
-
-#rpk redpanda admin config log-level set http -l trace --host $INTERNAL_IP:9644
 
 sudo -u redpanda rpk cluster config set cloud_storage_bucket prefix-redpanda-si-bucket --api-urls $INTERNAL_IP:9644
 sudo -u redpanda rpk cluster config set cloud_storage_region us-west-2 --api-urls $INTERNAL_IP:9644
@@ -29,6 +26,9 @@ sudo -u redpanda rpk cluster config set cloud_storage_enable_remote_read true --
 sudo -u redpanda rpk cluster config set cloud_storage_enable_remote_write true --api-urls $INTERNAL_IP:9644
 sudo -u redpanda rpk cluster config set cloud_storage_cache_size $((2*1024*1024*1024)) --api-urls $INTERNAL_IP:9644
 sudo -u redpanda rpk cluster config set cloud_storage_segment_max_upload_interval_sec 30 --api-urls $INTERNAL_IP:9644
+sudo -u redpanda rpk cluster config set cloud_storage_credentials_source aws_instance_metadata --api-urls $INTERNAL_IP:9644
 sudo -u redpanda rpk cluster config set cloud_storage_enabled true --api-urls $INTERNAL_IP:9644
 
 systemctl restart redpanda
+
+sudo -u redpanda rpk redpanda admin config log-level set s3 -l trace -e 0 --host $INTERNAL_IP:9644
