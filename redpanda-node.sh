8e8d063afedbd4c639d1173790cc055767d4ed05
Josh Purcell <hello@vuld.in>
2022-10-24 10:49:49 -0400

    use INTERNAL_IP env var

--- redpanda-node.sh
+++ redpanda-node.sh
@@ -7,9 +7,6 @@ set -ex
 curl -1sLf https://packages.vectorized.io/sMIXnoa7DK12JW4A/redpanda/cfg/setup/bash.deb.sh | sudo -E bash
 apt install -y redpanda
 
-# ensure redpanda user is owner of all related files/directories
-find /etc/redpanda/ /var/lib/redpanda/ -name '*' | xargs -d '\n' chown redpanda:redpanda
-
 # configure redpanda
 INTERNAL_IP=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
 
@@ -20,14 +17,18 @@ sudo -u redpanda rpk redpanda config set organization 'test-org'
 sudo -u redpanda rpk redpanda config set redpanda.advertised_kafka_api "[{address: $INTERNAL_IP, port: 9092}]"
 sudo -u redpanda rpk redpanda config set redpanda.advertised_rpc_api "{address: $INTERNAL_IP, port: 33145}"
 
-sudo -u redpanda rpk cluster config set cloud_storage_bucket prefix-redpanda-si-bucket --api-urls 34.220.95.159:9644
-sudo -u redpanda rpk cluster config set cloud_storage_region us-west2 --api-urls 34.220.95.159:9644
-sudo -u redpanda rpk cluster config set cloud_storage_access_key ABCDEFGHIJKLMNOP --api-urls 34.220.95.159:9644
-sudo -u redpanda rpk cluster config set cloud_storage_secret_key 1234567890abcdefghijklmnop --api-urls 34.220.95.159:9644
-sudo -u redpanda rpk cluster config set cloud_storage_enable_remote_read true --api-urls 34.220.95.159:9644
-sudo -u redpanda rpk cluster config set cloud_storage_enable_remote_write true --api-urls 34.220.95.159:9644
-sudo -u redpanda rpk cluster config set cloud_storage_segment_max_upload_interval_sec 30 --api-urls 34.220.95.159:9644
-sudo -u redpanda rpk cluster config set cloud_storage_enabled true --api-urls 34.220.95.159:9644
-
-# start redpanda
 systemctl start redpanda
+
+#rpk redpanda admin config log-level set http -l trace --host $INTERNAL_IP:9644
+
+sudo -u redpanda rpk cluster config set cloud_storage_bucket prefix-redpanda-si-bucket --api-urls $INTERNAL_IP:9644
+sudo -u redpanda rpk cluster config set cloud_storage_region us-west-2 --api-urls $INTERNAL_IP:9644
+sudo -u redpanda rpk cluster config set cloud_storage_access_key ABCDEFGHIJKLMNOP --api-urls $INTERNAL_IP:9644
+sudo -u redpanda rpk cluster config set cloud_storage_secret_key 1234567890abcdefghijklmnop --api-urls $INTERNAL_IP:9644
+sudo -u redpanda rpk cluster config set cloud_storage_enable_remote_read true --api-urls $INTERNAL_IP:9644
+sudo -u redpanda rpk cluster config set cloud_storage_enable_remote_write true --api-urls $INTERNAL_IP:9644
+sudo -u redpanda rpk cluster config set cloud_storage_cache_size $((2*1024*1024*1024)) --api-urls $INTERNAL_IP:9644
+sudo -u redpanda rpk cluster config set cloud_storage_segment_max_upload_interval_sec 30 --api-urls $INTERNAL_IP:9644
+sudo -u redpanda rpk cluster config set cloud_storage_enabled true --api-urls $INTERNAL_IP:9644
+
+systemctl restart redpanda
